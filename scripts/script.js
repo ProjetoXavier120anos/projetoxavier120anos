6010a0e79bd610aa7d51762b2bebd2ad37c44714
otaviooliveira0099@gmail.com <otaviooliveira0099@gmail.com>
2023-09-29 11:49:51 -0300

    Modificado o array cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 let cards = [
       {
-        titulo: 'Tsoro Yematatu',
-        descricao: 'O Tsoro Yematatu é um jogo tradicional do Zimbábue. O objetivo é alinhar três peças em uma linha reta para ganhar.',
-        imagem: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj6SAfrqqtkWyFQSNKovzbjlp8ZUfkk5BozVjeyVXyKY7eazMckrxWYGYA-qW9W_lmCmrokMhDqamvQF0WA1PgQfsdGOPFPsjPIKGVV5ndlP93LvlHIsf2UubZmtlK9a4MaLLw7PDuebTlPsAubBs2NWJOGUPDhHyypUMiWV_GgGadsu4boUqPi-Z6vqg/s778/Tsoro%20Yematatu%202.jpg',
-        link: '/jogo/tsoro-yematatu/'
+        titulo: `Tsoro Yematatu`,
+        descricao: `O Tsoro Yematatu é um jogo tradicional do Zimbábue. O objetivo é alinhar três peças em uma linha reta para ganhar.`,
+        imagem: `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj6SAfrqqtkWyFQSNKovzbjlp8ZUfkk5BozVjeyVXyKY7eazMckrxWYGYA-qW9W_lmCmrokMhDqamvQF0WA1PgQfsdGOPFPsjPIKGVV5ndlP93LvlHIsf2UubZmtlK9a4MaLLw7PDuebTlPsAubBs2NWJOGUPDhHyypUMiWV_GgGadsu4boUqPi-Z6vqg/s778/Tsoro%20Yematatu%202.jpg`,
+        link: `/jogo/tsoro-yematatu/`
     },
     
       {
-        titulo: 'Morabaraba',
-        descricao: 'Morabaraba é um jogo de estratégia com tabuleiro. É uma variação
+        titulo: `Morabaraba`,
+        descricao: `Morabaraba é um jogo de estratégia com tabuleiro. É uma variação
 do jogo inglês “Morris”, introduzida na África pelos colonizadores britânicos,
 tendo adquirido na variante africana as linhas diagonais e o aumento no
-número de peças.',
-        imagem: 'https://image.winudf.com/v2/image1/Y29tLm1pbGxnYW1lLmFsaWduaXRfaWNvbl8xNTc3OTgyNDc4XzAxMw/icon.png?w=&fakeurl=1',
-        link: '/jogo/morabaraba/'
+número de peças.`,
+        imagem: `https://image.winudf.com/v2/image1/Y29tLm1pbGxnYW1lLmFsaWduaXRfaWNvbl8xNTc3OTgyNDc4XzAxMw/icon.png?w=&fakeurl=1`,
+        link: `/jogo/morabaraba/`
       },
     //{...}
     ]
@@ -34,8 +34,8 @@ número de peças.',
     return cardInnerHTML;
 }
 
-window.addEventListener('load', () => {
-    const cardContainer = document.querySelector('.card-container > div');
+window.addEventListener(`load`, () => {
+    const cardContainer = document.querySelector(`.card-container > div`);
 
     cards.forEach(card => {
         cardContainer.innerHTML += criarCard(card.titulo, card.descricao, card.imagem, card.link);
