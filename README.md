77f1e2b42344f88335caa863fa3ca1bb46bc7625
Otávio Oliveira <otaviooliveira0099@gmail.com>
2023-10-07 13:10:15 +0000

    Modificado

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ O projeto em comemoração aos 120 anos do Colégio Estadual Dr. Xavier da Silva
 O presente site foi concebido por Otávio Oliveira, aluno do 3° ano E. Seu escopo central é fornecer um espaço dedicado à hospedagem dos manuais concernentes aos jogos e atividades lúdicas que foram desenvolvidos pela mencionada turma. Essas iniciativas representam uma parte fundamental do projeto voltado à escola.
 
 A plataforma digital em questão não apenas reflete a dedicação e a criatividade dos envolvidos, mas também serve como um repositório valioso para compartilhar os procedimentos e diretrizes dos jogos e brincadeiras criados. Dessa maneira, ela contribui de maneira significativa para o enriquecimento da experiência educacional oferecida pela instituição no âmbito desse projeto emblemático.
+
+Créditos:
+Javascript QR Encoder, Copyright 2010, tz@execpc.com, released under GPLv3
--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,8 @@ Mehen é um elemento presente na cultura egípcia; refere-se a um deus cobra que
       },
     //{...}
     ]
+
+  // Função criarCard retorna o HTML de um card
   function criarCard(titulo, descricao, imagem, link) {
     let cardInnerHTML = `
     <div class="card col">
@@ -25,6 +27,7 @@ Mehen é um elemento presente na cultura egípcia; refere-se a um deus cobra que
         </h5>
         <p class="card-text">${descricao}</p>
         <a href="${link}" class="btn btn-primary">Como jogar?</a>
+        <a href="#" onclick="desenharQrCode('${link}', '${titulo}')" class="float-end"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-520v-320h320v320H120Zm80-80h160v-160H200v160Zm-80 480v-320h320v320H120Zm80-80h160v-160H200v160Zm320-320v-320h320v320H520Zm80-80h160v-160H600v160Zm160 480v-80h80v80h-80ZM520-360v-80h80v80h-80Zm80 80v-80h80v80h-80Zm-80 80v-80h80v80h-80Zm80 80v-80h80v80h-80Zm80-80v-80h80v80h-80Zm0-160v-80h80v80h-80Zm80 80v-80h80v80h-80Z"/></svg></a>
       </div>
     </div>
     `;
@@ -32,11 +35,66 @@ Mehen é um elemento presente na cultura egípcia; refere-se a um deus cobra que
     return cardInnerHTML;
 }
 
+// Declaração de variáveis globais
+let tituloDoJogo, linkCompletoDoJogo;
+
+// Função para fazer o download da imagem da canvas como PNG
+function downloadCanvas() {
+  // Obtém o elemento da canvas
+  const canvas = document.getElementById('qrcanvas');
+  const link = document.createElement('a');
+
+  // Converte o conteúdo da canvas para uma URL de dados (PNG)
+  const image = canvas.toDataURL('image/png');
+
+  // Define a URL de dados como o link de download
+  link.href = image;
+
+  // Define o nome do arquivo para o download
+  link.download = `qrcode_${tituloDoJogo}.png`;
+
+  // Clica no link para iniciar o download
+  link.click();
+}
+
+// Função para desenhar o código QR
+function desenharQrCode(link, titulo) {
+  // Constrói o link completo do jogo
+  linkCompletoDoJogo = `https://projetoxavier120anos.github.io${link}`;
+  tituloDoJogo = titulo;
+
+  // Função doqr desenha o código QR
+  doqr(linkCompletoDoJogo);
+  // Chama a função para mostrar o modal com o QR Code
+  QrModal();
+}
+
+// Variável para controlar se o modal está aberto ou fechado
+let open = false;
+
+// Função para controlar a exibição do modal com o QR Code
+function QrModal() {
+  const dialogQrCode = document.querySelector('.dialog-qrcode');
+
+  // Alterna o estado do modal (aberto/fechado)
+  open = !open;
+
+  if (open)
+    dialogQrCode.showModal(); // Mostra o modal
+  else
+    dialogQrCode.close(); // Fecha o modal
+}
+
+// Evento de carregamento da página
 window.addEventListener(`load`, () => {
+    // Função setupqr inicia a o QrCode
+    setupqr();
+
     const cardContainer = document.querySelector(`.card-container > div`);
 
+    // Itera sobre as cartas e cria os cards na interface
     cards.forEach(card => {
         cardContainer.innerHTML += criarCard(card.titulo, card.descricao, card.imagem, card.link);
     });
-
 });
+
